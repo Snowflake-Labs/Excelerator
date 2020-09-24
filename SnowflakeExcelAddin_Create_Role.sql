4ef0c56ecec2d43bb03080cf1b8f0b3d676986a9
Steven Segal <StevenSegal@gmail.com>
2020-09-24 15:05:27 -0500

    Simplified Create Role script

--- SnowflakeExcelAddin_Create_Role.sql
+++ SnowflakeExcelAddin_Create_Role.sql
@@ -1,34 +1,20 @@
 /* ----------------------- Snowflake Excel Integration ------------------------
-    Run this script to set up permission for user that will use Snowflake Excelerator
-	To run this script, use a role with elvated permissions such as ACCOUNTADMIN
-	This scripts create a role named ExcelAnalyst that will be granted to another role passed in: <parentrole>. This is the role that the user can login with.
+    This script is only needed when updating Snowflake from Excel. 
+	An alternative to running this script, is to grant the user's role the 'Create Stage' and 'Create Table' privileges on the schema they will login to.
+
+	This script allows for update from Excel without changing the user's role directly. Instead, a new role is created, 'ExcelAnalyst',  and assigned to the user's role.
+	To allow multiple roles to have update access, run this script one time for the first role and then assign the 'ExcelAnalyst' role manually to the other roles.
+	
+	To run this script, use either role ACCOUNTADMIN or SECURITYADMIN
+	This script creates a role named 'ExcelAnalyst' that will be granted to the role specified below as 'existingUserRole'. This is the role that has been granted to the Excel user.
 	Before running this scripts update the following placeholders with values specific to your environment
-	<role>, <database>, <schema>, <warehouse>
-	
-	Certain privileges are needed for certain capabilities 
-	This script does not deal with table level privileges, only schema and database level. Ideally the ParenRole will have the table privileges
-	Here are the privileges needed for each capability:
-	Query    			
-		Database: 	USAGE
-		Schema:		USAGE, CREATE STAGE - Unless a Stage is provided in the login, then the user will need USAGE to that Stage
-		Table: 		SELECT
-	Upload (everything in Query plus)
-		Schema: 	CREATE TABLE
-		Table: 		INSERT, UPDATE, TRUNCATE			
-	Rollback (Schema level privileges above plus)
-		Table: 		Ownership	 
-	
-	For advanced feature: Auto-generate Data Types
-		usage on all procedures
-		usage on future procedures
-		usage on all functions
-		usage on future functions
+	<existingUserRole>, <database>, <schema>, <warehouse>
 	
 */
 
 -------------- Update this section -----------------------------------
 -- User name and role that the user will login with 
-set parentUserRole = '<parentrole>';
+set existingUserRole = '<existingUserRole>';
  -- database and schema where you will create the Snowflake Excel Integration stored procedures
 set databaseName = '<database>';
 set schemaName = '<schema>';
@@ -39,15 +25,17 @@ set roleName = 'ExcelAnalyst';
 set databaseAndSchema = concat($databaseName,'.',$schemaName);
 
 create role IF NOT EXISTS IDENTIFIER($roleName);
-grant role IDENTIFIER($roleName) to role IDENTIFIER($parentUserRole);
-grant usage on database IDENTIFIER($databaseName)  to role IDENTIFIER($roleName);
-grant usage on schema IDENTIFIER($databaseAndSchema) to role IDENTIFIER($roleName);
+grant role IDENTIFIER($roleName) to role IDENTIFIER($existingUserRole);
+
 grant create stage on schema IDENTIFIER($databaseAndSchema) to role IDENTIFIER($roleName);
+-- For rollback functionality
 grant create table on schema IDENTIFIER($databaseAndSchema) to role IDENTIFIER($roleName);
 
-grant usage on warehouse IDENTIFIER($warehouseName) to IDENTIFIER($roleName);
-
--- For advanced feature: Auto-generate Data Types 
+------------------------------------ For advanced feature: Auto-generate Data Types ------------------------------------ 
+-- Grant access to the DB and Schema where stored procs are created
+grant usage on database IDENTIFIER($databaseName)  to role IDENTIFIER($roleName);
+grant usage on schema IDENTIFIER($databaseAndSchema) to role IDENTIFIER($roleName);
+-- Grant access to the stored procs
 grant usage on future procedures in schema IDENTIFIER($databaseAndSchema) to role IDENTIFIER($roleName);
 grant usage on future functions in schema IDENTIFIER($databaseAndSchema) to role IDENTIFIER($roleName);
 grant usage on all procedures in schema IDENTIFIER($databaseAndSchema) to role IDENTIFIER($roleName);
